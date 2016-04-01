c8fc4ad557153d003c08f13c7da2a2db1974ff5f
Edith Ho <edithho@calvisitor-10-105-159-98.calvisitor.1918.berkeley.edu>
2016-03-31 18:50:38 -0700

    added new code and dataset

--- Edith_models.R
+++ Edith_models.R
@@ -27,8 +27,47 @@ par(new=T)
 plot(Y[valid], col='firebrick1',type='l', ylab='')
 par(new=F)
 
+# accuracy
+MSE.lm <- (sum((Y[valid] - yhat_lm)^2))/length(yhat_lm)
+
 yhat_interval = predict(lm.mod, newdata = X[valid,], interval ='prediction')
 yhat_confit = predict(lm.mod, newdata = X[valid,], interview='confidence')
+
+## random forest
+# all predictors
+set.seed(1)
+dust.rf <- randomForest(ppd60_3~temperature+humidity+ppd42_1+ppd42_2
+                        +ppd42_3+ppd60_1+ppd60_2, data=dust, subset=train,
+                        mtry=7, ntree=25)
+dust.rf
+yhat.rf <- predict(dust.rf, newdata=X[valid,])
+plot(yhat.rf, type='l', main="Random Forest",
+     col='cornflowerblue',ylab="Values")
+par(new=T)
+plot(Y[valid], col='firebrick1',type='l', ylab='')
+par(new=F)
+
+
+mean((Y[valid] - yhat.rf)^2)
+plot(yhat.rf, Y[valid])
+abline(0,1)
+
+# without the two pp60
+dust.rf2 <- randomForest(ppd60_3~temperature+humidity+ppd42_1+ppd42_2
+                        +ppd42_3, data=dust, subset=train,
+                        mtry=5, ntree=25)
+dust.rf2
+yhat.rf2 <- predict(dust.rf2, newdata=X[valid,])
+plot(yhat.rf2, type='l', main="Random Forest",
+     col='cornflowerblue',ylab="Values")
+par(new=T)
+plot(Y[valid], col='firebrick1',type='l', ylab='')
+par(new=F)
+mean((Y[valid] - yhat.rf2)^2) #higher MSE but expected
+plot(yhat.rf2, Y[valid])
+abline(0,1)
+
+
 
 ## regression tree/random forest
 tree.dust <- tree(ppd60_3~temperature+humidity+ppd42_1+ppd42_2+ppd42_3, dust)
@@ -39,3 +78,5 @@ tree.dust
 
 tree.dust.train <- tree(ppd60_3~temperature+humidity+ppd42_1+ppd42_2+ppd42_3, dust, subset = train)
 tree.pred <- predict(tree.dust.train, dust[valid,])
+
+
