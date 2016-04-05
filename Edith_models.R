d0873535001385e0e6ea323073e03220f979590a
Edith Ho <edithho@airbears2-10-142-23-238.airbears2.1918.berkeley.edu>
2016-04-04 17:16:35 -0700

    added importance

--- Edith_models.R
+++ Edith_models.R
@@ -2,10 +2,11 @@ library(ggplot2)
 library(glmnet)
 library(tree)
 library(randomForest)
+library(boot)
 
 ## linear regression
-dust <- read.csv('/Users/edithho/Downloads/Dust Sensor Comparison.csv')
-pairs(dust[,4:11])
+dust <- read.csv('/Users/edithho/Google Drive/cal/2016 spring/air-quality-sensor_testing/Dust Sensor Comparison.csv')
+# pairs(dust[,4:11])
 head(dust)
 X = dust[,4:10]
 Y = dust$ppd60_3
@@ -20,7 +21,7 @@ lm.mod2 = lm(ppd60_3~temperature+humidity+ppd42_1+ppd42_2+ppd42_3, dust[train,])
 summary(lm.mod2)
 plot(lm.mod2)
 
-yhat_lm = predict(lm.mod, newdata = X[valid,])
+yhat_lm = predict(lm.mod2, newdata = X[valid,])
 plot(yhat_lm, type='l', main="Linear Regression",
      col='cornflowerblue',ylab="Values")
 par(new=T)
@@ -29,9 +30,18 @@ par(new=F)
 
 # accuracy
 MSE.lm <- (sum((Y[valid] - yhat_lm)^2))/length(yhat_lm)
-
+MSE.lm 
 yhat_interval = predict(lm.mod, newdata = X[valid,], interval ='prediction')
 yhat_confit = predict(lm.mod, newdata = X[valid,], interview='confidence')
+
+lm.mod3 = lm(ppd60_3~temperature+humidity+ppd42_1+ppd42_2+ppd42_3+ppd60_1+ppd60_2, dust[train,])
+summary(lm.mod3)
+MSE.lm3 <- (sum((Y[valid] - predict(lm.mod3, newdata = X[valid,]))^2))/length(Y[valid])
+MSE.lm3
+
+
+glm.fit <- glm(ppd60_3~temperature+humidity+ppd42_1+ppd42_2+ppd42_3, dust[train,])
+cv.err <- cv.glm(dust[train,], glm.fit)
 
 ## random forest
 # all predictors
@@ -46,7 +56,8 @@ plot(yhat.rf, type='l', main="Random Forest",
 par(new=T)
 plot(Y[valid], col='firebrick1',type='l', ylab='')
 par(new=F)
-
+importance(dust.rf)
+varImpPlot(dust.rf)
 
 mean((Y[valid] - yhat.rf)^2)
 plot(yhat.rf, Y[valid])
@@ -66,7 +77,8 @@ par(new=F)
 mean((Y[valid] - yhat.rf2)^2) #higher MSE but expected
 plot(yhat.rf2, Y[valid])
 abline(0,1)
-
+importance(dust.rf2)
+varImpPlot(dust.rf2)
 
 
 ## regression tree/random forest
