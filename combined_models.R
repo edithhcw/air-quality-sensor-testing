491c64252e6914486d7da7386857c5c4f53c776f
Edith Ho <edithho@airbears2-10-142-23-197.airbears2.1918.berkeley.edu>
2016-04-08 16:07:32 -0700

    added new file with all models

--- combined_models.R
+++ combined_models.R
@@ -1,11 +1,18 @@
+# libraries
 library(ggplot2)
 library(glmnet)
 library(tree)
 library(randomForest)
 library(boot)
+library(quantreg)
+library (ISLR)
+library(plsdof)
+library(pls)
+set.seed(1)
 
-## linear regression
-dust <- read.csv('/Users/edithho/Google Drive/cal/2016 spring/air-quality-sensor_testing/Dust Sensor Comparison.csv')
+# loading in data
+dust <- read.csv('/Users/edithho/Google Drive/cal/
+                 2016 spring/air-quality-sensor_testing/Dust Sensor Comparison.csv')
 # pairs(dust[,4:11])
 head(dust)
 X = dust[,4:10]
@@ -13,6 +20,9 @@ Y = dust$ppd60_3
 n = nrow(X)
 train = sample(1:nrow(dust), (nrow(dust) * .7))
 valid = -train
+
+## linear regression
+
 
 lm.mod = lm(ppd60_3~temperature+humidity+ppd42_1+ppd42_2+ppd42_3, dust)
 summary(lm.mod)
@@ -45,7 +55,7 @@ cv.err <- cv.glm(dust[train,], glm.fit)
 
 ## random forest
 # all predictors
-set.seed(1)
+
 dust.rf <- randomForest(ppd60_3~temperature+humidity+ppd42_1+ppd42_2
                         +ppd42_3+ppd60_1+ppd60_2, data=dust, subset=train,
                         mtry=7, ntree=25)
@@ -65,8 +75,8 @@ abline(0,1)
 
 # without the two pp60
 dust.rf2 <- randomForest(ppd60_3~temperature+humidity+ppd42_1+ppd42_2
-                        +ppd42_3, data=dust, subset=train,
-                        mtry=5, ntree=25)
+                         +ppd42_3, data=dust, subset=train,
+                         mtry=5, ntree=25)
 
 dust.rf3 <- randomForest(dust[train,c(4:8)], dust$ppd60_3[train],
                          mtry=5, ntree=25)
@@ -96,3 +106,68 @@ tree.dust.train <- tree(ppd60_3~temperature+humidity+ppd42_1+ppd42_2+ppd42_3, du
 tree.pred <- predict(tree.dust.train, dust[valid,])
 
 
+## jim's
+
+d <- dust[,4:11]
+pairs(dust[,4:11])
+
+X = dust[,4:10]
+Y = dust$ppd60_3
+n = nrow(X)
+train = 1:round(n*.7)
+valid = round(n*.7 + 1):n
+
+#PCR
+
+pcr.fit=pcr(ppd60_3~., data=d, subset=train, scale=TRUE, validation="CV")
+res = MSEP(pcr.fit)
+pcr.best=which.min(res$val[1,,])-1
+cv = res$val[,,pcr.best+1]
+pcr.pred=predict(pcr.fit, X[valid,], ncomp=pcr.best)
+MSE <- (sum((Y[valid] - pcr.pred)^2))/length(pcr.pred)
+print(MSE)
+
+#L1 Regression
+model.rq <- rq(ppd60_3~temperature+humidity+ppd42_1+ppd42_2+ppd42_3, data = dust[train,], tau = .5)
+print(model.rq)
+pre = predict(model.rq, newdata=X[valid,])
+MSE2 <- (sum((test$ppd60_3 - pre)^2))/length(pre)
+MSE2
+
+##yc
+###ridge
+grid=10^seq(-1,-3,length=100)
+ridge.mod=glmnet(x,y,alpha=0,lambda=grid)
+dim(coef(ridge.mod))  #6*100
+
+#cv
+train=sample(1:nrow(x), nrow(x)/2)
+test=(-train)
+y.test=y[test]
+
+cv.out=cv.glmnet(x[train,],y[train],alpha=0)
+plot(cv.out)
+bestlam1=cv.out$lambda.min #bestlam:lambda that results in the smallest cv error
+
+ridge.pred=predict(ridge.mod,s=bestlam1,newx=x[test,])
+mean((ridge.pred-y.test)^2) # MSE associated with the best lambda
+mean(abs(ridge.pred-y.test)) # MAE associated with the best lambda
+out=glmnet(x,y,alpha=0)
+predict(out,type="coefficients",s=bestlam1)[1:6,]
+
+
+###lasso
+lasso.mod=glmnet(x[train,],y[train],alpha=1,lambda=grid)
+dim(coef(lasso.mod))
+plot(lasso.mod)
+
+cv.out=cv.glmnet(x[train,],y[train],alpha=1)
+plot(cv.out)
+bestlam2=cv.out$lambda.min
+lasso.pred=predict(lasso.mod,s=bestlam2,newx=x[test,])
+mean((lasso.pred-y.test)^2) #MSE
+mean(abs(lasso.pred-y.test)) #MAE
+out=glmnet(x,y,alpha=1,lambda=grid)
+lasso.coef=predict(out,type="coefficients",s=bestlam2)[1:6,]
+lasso.coef
+lasso.coef[lasso.coef!=0]
