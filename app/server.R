3150b6c2e115f7729fa0c723f361b2c089d99c4f
edithhcw <edithhcw@berkeley.edu>
2016-04-25 18:23:04 -0700

    adding circles

--- app/server.R
+++ app/server.R
@@ -6,6 +6,7 @@ library(leaflet)
 library(data.table)
 counties <- readRDS("data/counties.rds")
 zip <- read.csv("data/zipcode.csv")
+sites <- read.csv('data/aqs_sites.csv')
 source("helpers.R")
 top = 49.3457868 # north lat
 left = -124.7844079 # west long
@@ -18,6 +19,18 @@ w=c(zip$zip_code)
 dt = data.table(w, val = w)
 setattr(dt, "sorted", "w") 
 #print(dt[J(x), .I, roll = "nearest", by = .EACHI])
+
+
+cities <- read.csv(textConnection("City,Lat,Long,Pop
+                                  Boston,42.3601,-71.0589,645966
+                                  Hartford,41.7627,-72.6743,125017
+                                  New York City,40.7127,-74.0059,8406000
+                                  Philadelphia,39.9500,-75.1667,1553000
+                                  Pittsburgh,40.4397,-79.9764,305841
+                                  Providence,41.8236,-71.4222,177994
+                                  "))
+
+
 getIndex <- function(zcode){
   index = dt[J(zcode), .I, roll = "nearest", by = .EACHI][[2]]
   return(index)
@@ -40,7 +53,11 @@ shinyServer(
         ) %>%
         setView(lng = -93.85, lat = 37.45, zoom = 4)
     })
-
+    
+    leaflet(cities) %>% addTiles() %>%
+      addCircles(lng = ~Long, lat = ~Lat, weight = 1,
+                 radius = ~sqrt(Pop) * 30, popup = ~City
+      )
     observe({
       zcode <- input$num
       if(!is.na(zcode)){
@@ -80,3 +97,4 @@ shinyServer(
     })
   }
 )
+
