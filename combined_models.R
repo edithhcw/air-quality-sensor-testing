f05030390c9ee4cf1e32858ef5d02f9918d74a90
edithhcw <edithhcw@berkeley.edu>
2016-05-01 22:13:47 -0700

    added plots

--- combined_models.R
+++ combined_models.R
@@ -24,25 +24,23 @@ valid = -train
 
 
 # Machine Learning Models
-# L2 linear regression
-l2LinearReg <- function(dust, X, Y, train, valid, relation) {
-	lm.mod = lm(relation, dust)
+# linear regression
+LinearReg <- function(dust, X, Y, train, valid, relation) {
 	lm.mod2 = lm(relation, dust[train,])
 	yhat_lm = predict(lm.mod2, newdata = X[valid,])
 	plot(yhat_lm, type = 'l', main = "Linear Regression",
      	col = 'cornflowerblue',ylab = "Values")
 	par(new = T)
-	plot(Y[valid], col = 'firebrick1', type = 'l', ylab = '')
+	plot(Y[valid], col = 'orange', type = 'l', ylab = '', xaxt='n', yaxt='n')
+	par(new = T)
+	plot(X[,3], col = 'firebrick1', type = 'l', ylab = '', xaxt='n', yaxt='n')
+	legend("topleft",c('Predicted', 'Expensive', 'Cheap'), bty = "n",
+	       lty=c(1,1),lwd=c(2.5,2.5),col=c('cornflowerblue', 'orange', 'firebrick1')) 
 	par(new = F)
 
 	MSE.lm = (sum((Y[valid] - yhat_lm)^2)) / length(yhat_lm)
-	yhat_interval = predict(lm.mod, newdata = X[valid,], interval = 'prediction')
-	yhat_confit = predict(lm.mod, newdata = X[valid,], interview = 'confidence')
-
-	lm.mod3 = lm(relation, dust[train,])
-	MSE.lm3 = (sum((Y[valid] - predict(lm.mod3, newdata = X[valid,]))^2)) / length(Y[valid])
 	
-	ret = list("model" = lm.mod3, "error" = MSE.lm3, "name" = "L2 Linear Regression")
+	ret = list("model" = lm.mod2, "error" = MSE.lm, "name" = "Linear Regression")
 	return(ret)
 }
 
@@ -52,9 +50,13 @@ RanForest <- function(dust, X, Y, train, valid, relation) {
 	                        ntree = 100)
 	yhat.rf = predict(dust.rf, newdata = X[valid,])
 	plot(yhat.rf, type = 'l', main = "Random Forest",
-     	col = 'cornflowerblue', ylab = "Values")
+	     col = 'cornflowerblue',ylab = "Values")
 	par(new = T)
-	plot(Y[valid], col = 'firebrick1', type = 'l', ylab = '')
+	plot(Y[valid], col = 'orange', type = 'l', ylab = '', xaxt='n', yaxt='n')
+	par(new = T)
+	plot(X[,3], col = 'firebrick1', type = 'l', ylab = '', xaxt='n', yaxt='n')
+	legend("topleft",c('Predicted', 'Expensive', 'Cheap'), bty = "n",
+	       lty=c(1,1),lwd=c(2.5,2.5),col=c('cornflowerblue', 'orange', 'firebrick1')) 
 	par(new = F)
 	importance(dust.rf)
 	varImpPlot(dust.rf)
@@ -73,6 +75,15 @@ PCR <- function(dust, X, Y, train, valid, relation) {
 	cv = res$val[,,pcr.best+1]
 	pcr.pred = predict(pcr.fit, X[valid,], ncomp = pcr.best)
 	MSE.pcr = (sum((Y[valid] - pcr.pred)^2)) / length(pcr.pred)
+	plot(pcr.pred, type = 'l', main = "Principal Components Regression",
+	     col = 'cornflowerblue',ylab = "Values")
+	par(new = T)
+	plot(Y[valid], col = 'orange', type = 'l', ylab = '', xaxt='n', yaxt='n')
+	par(new = T)
+	plot(X[,3], col = 'firebrick1', type = 'l', ylab = '', xaxt='n', yaxt='n')
+	legend("topleft",c('Predicted', 'Expensive', 'Cheap'), bty = "n",
+	       lty=c(1,1),lwd=c(2.5,2.5),col=c('cornflowerblue', 'orange', 'firebrick1')) 
+	par(new = F)
 	
 	ret = list("model" = pcr.fit, "error" = MSE.pcr, "ncomp" = pcr.best,  "name" = "Principal Components Regression")
 	return(ret)
@@ -83,6 +94,15 @@ l1LinearReg <- function(dust, X, Y, train, valid, relation) {
 	model.rq = rq(relation, data = dust[train,], tau = .5)
 	pre = predict(model.rq, newdata = X[valid,])
 	MSE.l1reg = (sum((Y[valid] - pre)^2)) / length(pre)
+	plot(pre, type = 'l', main = "L1 Linear Regression",
+	     col = 'cornflowerblue',ylab = "Values")
+	par(new = T)
+	plot(Y[valid], col = 'orange', type = 'l', ylab = '', xaxt='n', yaxt='n')
+	par(new = T)
+	plot(X[,3], col = 'firebrick1', type = 'l', ylab = '', xaxt='n', yaxt='n')
+	legend("topleft",c('Predicted', 'Expensive', 'Cheap'), bty = "n",
+	       lty=c(1,1),lwd=c(2.5,2.5),col=c('cornflowerblue', 'orange', 'firebrick1')) 
+	par(new = F)
 	
 	ret = list("model" = model.rq, "error" = MSE.l1reg, "name" = "L1 Linear Regression")
 	return(ret)
@@ -97,6 +117,15 @@ Ridge <- function(dust, X, Y, train, valid) {
   	bestlam1 = cv.out$lambda.min
   	ridge.pred = predict(ridge.mod,s = bestlam1, newx = X[valid,])
   	MSE.ridge = mean((ridge.pred-Y[valid])^2)
+  	plot(ridge.pred, type = 'l', main = "Ridge Regression",
+  	     col = 'cornflowerblue',ylab = "Values")
+  	par(new = T)
+  	plot(Y[valid], col = 'orange', type = 'l', ylab = '', xaxt='n', yaxt='n')
+  	par(new = T)
+  	plot(X[,3], col = 'firebrick1', type = 'l', ylab = '', xaxt='n', yaxt='n')
+  	legend("topleft",c('Predicted', 'Expensive', 'Cheap'), bty = "n",
+  	       lty=c(1,1),lwd=c(2.5,2.5),col=c('cornflowerblue', 'orange', 'firebrick1')) 
+  	par(new = F)
   	
   	ret = list("model" = ridge.mod, "error" = MSE.ridge, "bestlam" = bestlam1,  "name" = "Ridge Regression")
   	return(ret)
@@ -110,19 +139,29 @@ Lasso <- function(dust, X, Y, train, valid) {
   	bestlam2 = cv.out$lambda.min
   	lasso.pred = predict(lasso.mod, s = bestlam2, newx = X[valid,])
   	MSE.lasso = mean((lasso.pred-Y[valid])^2)
+  	plot(lasso.pred, type = 'l', main = "LASSO Regression",
+  	     col = 'cornflowerblue',ylab = "Values")
+  	par(new = T)
+  	plot(Y[valid], col = 'orange', type = 'l', ylab = '', xaxt='n', yaxt='n')
+  	par(new = T)
+  	plot(X[,3], col = 'firebrick1', type = 'l', ylab = '', xaxt='n', yaxt='n')
+  	legend("topleft",c('Predicted', 'Expensive', 'Cheap'), bty = "n",
+  	       lty=c(1,1),lwd=c(2.5,2.5),col=c('cornflowerblue', 'orange', 'firebrick1')) 
+  	par(new = F)
 
 	ret <- list("model" = lasso.mod, "error" = MSE.lasso, "bestlam" = bestlam2,  "name" = "Lasso Regression")
   	return(ret)
 }
 
+
 relation = formula(ppd60_3 ~ temperature + humidity + ppd42_1)
-result.l2 = l2LinearReg(dust, X, Y, train, valid, relation)
+result.lm = LinearReg(dust, X, Y, train, valid, relation)
 result.rf = RanForest(dust, X, Y, train, valid, relation)
 result.pcr = PCR(dust, X, Y, train, valid, relation)
 result.l1 = l1LinearReg(dust, X, Y, train, valid, relation)
 result.ridge = Ridge(dust, X, Y, train, valid)
 result.lasso = Lasso(dust, X, Y, train, valid)
-result = list(result.l2,　result.rf, result.pcr, result.l1, result.ridge, result.lasso)
+result = list(result.lm,　result.rf, result.pcr, result.l1, result.ridge, result.lasso)
 min = result[[1]]$error
 index = 1
 for (i in 1:6){
