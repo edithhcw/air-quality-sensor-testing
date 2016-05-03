e4d1d15bfeabeb6bfc0cb8c307489403d609fb7a
Jim Bai <jimbai@berkeley.edu>
2016-05-02 22:43:15 -0700

    upload map application

--- app/server.R
+++ app/server.R
@@ -4,62 +4,112 @@ library(maps)
 library(mapproj)
 library(leaflet)
 library(data.table)
-counties <- readRDS("data/counties.rds")
+library(httr)
+library(foreign)
 zip <- read.csv("data/zipcode.csv")
+#statefull <- read.csv("data/stateabb.csv")
 sites <- read.csv('data/aqs_sites.csv')
+aqicolor <- read.csv('data/legend.csv')
 sites_small <- sites[,c("Latitude", "Longitude", 'City.Name', 'State.Name')]
+
 sites_small <- na.omit(sites_small)
 sites_small$readings <- sample(c(0:500), nrow(sites_small), replace = T)
-sites_ca <- sites_small[sites_small$State.Name == 'California',]
-sites_ny <- sites_small[sites_small$State.Name == 'New York',]
-source("helpers.R")
+#sites_ny <- sites_small[sites_small$State.Name == 'New York',]
 top = 49.3457868 # north lat
 left = -124.7844079 # west long
 right = -66.9513812 # east long
 bottom =  24.7433195 # south lat
-x=94702
+
 w=c(zip$zip_code)
-#dt = data.table(zip$zip_code, val = zip$zip_code)
-#setattr(dt, "sorted", "zip$zip_code")
 dt = data.table(w, val = w)
 setattr(dt, "sorted", "w") 
-#print(dt[J(x), .I, roll = "nearest", by = .EACHI])
 
 getIndex <- function(zcode){
   index = dt[J(zcode), .I, roll = "nearest", by = .EACHI][[2]]
   return(index)
   }
+
+getZipData <- function(zcode){
+  url <- "https://airnowapi.org/aq/observation/zipCode/current/?"
+  format <- "text/csv"
+  zipCode <- zcode
+  api_key <- "3DF88662-6375-4BCE-A7EC-68DED28CEE2A"
+  req <- GET(url, query = list(format=format, zipCode = zipCode, api_key=api_key))
+  result1 <- content(req, "text")
+  writeBin(result1, "location.csv")
+  reading <- read.csv("location.csv")
+  reading <- na.omit(reading)
+  aqi <- reading$AQI
+  #print((length(aqi) == 0) && (typeof(aqi) == "logical"))
+  if((length(aqi) == 0) && (typeof(aqi) == "logical")) {
+    url <- "http://www.airnowapi.org/aq/forecast/zipCode/?"
+    format <- "text/csv"
+    zipCode <- zcode
+    api_key <- "3DF88662-6375-4BCE-A7EC-68DED28CEE2A"
+    distance <- 25
+    req <- GET(url, query = list(format=format, zipCode = zipCode, api_key=api_key, distance = distance))
+    result1 <- content(req, "text")
+    writeBin(result1, "pred.csv")
+    pred <- read.csv("pred.csv")
+    pm25 <- pred[pred$ParameterName == 'PM2.5']
+    print(pm25$AQI)
+    if((length(aqi) == 0) && (typeof(aqi) == "logical")) {
+      return(-1)
+    }
+    return(pm25$AQI)
+  }
+  #print(aqi)
+  return(aqi)
+  #print(head(reading))
+}
+getStateData <- function(state, lat, lng){
+  url <- "https://airnowapi.org/aq/data/"
+  parameters <- "pm25"
+  #left <- lng - 5
+  #right <- lng + 5
+  #up <- lat + 5
+  #down <- lat - 5
+  bbox <- paste(toString(left), toString(bottom), toString(right), toString(top), sep = ',')
+  #"-90.806632,24.634217,-71.119132,45.910790"
+  #print(bbox)
+  data_type <- "a"
+  format <- "text/csv"
+  ext <- "csv"
+  api_key <- "3DF88662-6375-4BCE-A7EC-68DED28CEE2A"
+  #REQUEST_URL <- paste(url, "?&parameters=" , parameters , "&bbox=" , bbox, "&datatype=", data_type, "&format=" , format, "&api_key=" ,api_key, sep="")
+  req <- GET(url, query = list(parameters= parameters , bbox= bbox, datatype= data_type, format=format, api_key=api_key))
+  #r <- GET(REQUEST_URL)
+  result1 <- content(req, "text")
+  result1 <- paste("latitude, longtitude, data, type, value, color", result1, sep = '\n')
+  writeBin(result1, "state.csv")
+}
 shinyServer(
   function(input, output) {
 
-    
-    # This reactive expression represents the palette function,
-    # which changes as the user makes selections in UI.
-    colorpal <- reactive({
-      colorNumeric(input$colors, sites_small$readings)
-    })
-    pal <- colorpal()
-    
+    pal <- colorFactor(c("chartreuse", "yellow", "orange", "red", "Purple", "Maroon"), domain = c("Good", "Moderate", "Unhealthy for Sensitive Groups", "Unhealthy", "Very Unhealthy", "Hazardous"))
     output$map <- renderLeaflet({
       leaflet() %>%
         addTiles(
-          urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
           attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
         ) %>%
         setView(lng = -93.85, lat = 37.45, zoom = 4)
-    })
-    
-    observe({
-      pal <- colorpal()
       
-      leaflet(sites_small) %>% addTiles() %>%
-        addCircles(lng = ~Longitude, lat = ~Latitude, weight = 1,
-                   radius = 1000, popup = ~City.Name, color = "#777777", 
-                   fillColor = ~pal(readings), fillOpacity = 0.7
-        )
     })
-    
-    
+    observe({
+    leafletProxy("map", data = aqicolor) %>% addLegend(position = "bottomright",
+                                                   title = " ", pal = pal,values = ~color)
+    })
+
+    pal <- colorFactor(c("chartreuse", "yellow", "orange", "red", "Purple", "Maroon"), domain = c(1, 2, 3, 4, 5, 6))
+    #getStateData(0, 0, 0)
+    site <- read.csv('state.csv');
+    site <- na.omit(site)
+    site <- site[site$value > 0,]
+    leafletProxy("map", data = site) %>% addTiles() %>%
+      addCircles(
+        lng = ~longtitude, lat = ~latitude, weight = 4,
+        radius = 10000, popup = ~paste("The value is", as.character(value), sep = ' '), color = ~pal(color), fillOpacity = 0.6
+      )
     observe({
       zcode <- input$num
       if(!is.na(zcode)){
@@ -67,9 +117,32 @@ shinyServer(
             index <- getIndex(zcode)
             lat <- zip$latitude[index]
             lng <- zip$longitude[index]
-            leafletProxy("map") %>% setView(lng, lat, zoom = 13)
+            stateabb <- zip[index,]$state
+            #aqi <- getZipData(zcode)
+            aqi <- -1
+            print(paste("the aqi is", aqi, sep = ' '))
+            
+            popup <- "not available"
+            if(0 > aqi) {
+              popup <- "not available"
+            } else if (aqi <= 50) {
+              popup <- "Good"
+            } else if(aqi <= 100) {
+              popup <- "Moderate"
+            } else if(aqi <= 150) {
+              popup <- "Unhealthy for Sensitive Groups"
+            } else if(aqi <= 200) {
+              popup <- "Unhealthy"
+            } else if(aqi <= 300) {
+              popup <- "Very Unhealthy"
+            } else if(aqi <= 500) {
+              popup <- "Hazardous"
+            }
+            
+            leafletProxy("map") %>% setView(lng, lat, zoom = 7)
             leafletProxy("map") %>% clearPopups
-            leafletProxy("map") %>% addPopups(lng, lat, "Here I am")
+            leafletProxy("map") %>% addPopups(lng, lat, paste("The air quality in your area is" , popup, sep = ' '))
+            
         } else {
           leafletProxy("map") %>% setView(lng = -93.85, lat = 37.45, zoom = 4)
         }
@@ -77,26 +150,7 @@ shinyServer(
         leafletProxy("map") %>% setView(lng = -93.85, lat = 37.45, zoom = 4)
       }
     })
-    # Incremental changes to the map (in this case, replacing the
-    # circles when a new color is chosen) should be performed in
-    # an observer. Each independent set of things that can change
-    # should be managed in its own observer.
-
     
-    # Use a separate observer to recreate the legend as needed.
-    observe({
-      proxy <- leafletProxy("map", data = sites_small)
-      
-      # Remove any existing legend, and only if the legend is
-      # enabled, create a new one.
-      proxy %>% clearControls()
-      if (input$legend) {
-        pal <- colorpal()
-        proxy %>% addLegend(position = "bottomright",
-                            pal = pal, values = ~readings
-        )
-      }
-    })
   }
 )
 
--- app/ui.R
+++ app/ui.R
@@ -6,13 +6,7 @@ shinyUI(
     tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
     leafletOutput("map", width = "100%", height = "100%"),
     absolutePanel(top = 10, right = 10,
-                  numericInput("num", label = h3("Zip Code"), value = 1),
-                  checkboxInput("legend", "Show legend", TRUE),
-                  selectInput("colors", "Color Scheme",
-                              rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
-                  )
-                  
-                  
+                  numericInput("num", label = h3("Zip Code"), value = 10000)
     )
   )
 )
